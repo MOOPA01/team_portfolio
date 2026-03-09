4aebb96bb7f582e88365e7853165e951bc7e101f
dragon-rg <rigved.me@gmail.com>
2026-03-08 22:42:21 -0700

    fixed bug that was causing L3 to break the code

--- assets/js/GameEnginev1/coconutL3.js
+++ assets/js/GameEnginev1/coconutL3.js
@@ -10,12 +10,12 @@
 // 3) Ensure images exist and paths resolve via 'path' provided by the engine.
 // 4) You can add more objects to this.classes inside the constructor.
 
-import GameEnvBackground from '/assets/js/GameEnginev1/essentials/GameEnvBackground.js';
-import Player from '/assets/js/GameEnginev1/essentials/Player.js';
-import Npc from '/assets/js/GameEnginev1/essentials/Npc.js';
-import Barrier from '/assets/js/GameEnginev1/essentials/Barrier.js';
+import GameEnvBackground from './essentials/GameEnvBackground.js';
+import Player from './essentials/Player.js';
+import Npc from './essentials/Npc.js';
+import Barrier from './essentials/Barrier.js';
 
-class GameLevelCoconutl3 {
+class Coconutl3 {
     constructor(gameEnv) {
         const path = gameEnv.path;
         const width = gameEnv.innerWidth;
@@ -23,33 +23,33 @@ class GameLevelCoconutl3 {
 
         const bgData = {
             name: "custom_bg",
-            src: path + "/images/gamebuilder/bg/clouds.jpg",
+            src: path + "/images/gamebuilder/bg/map_boss.jpg",
             pixels: { height: 720, width: 1280 }
         };
 
         const playerData = {
             id: 'playerData',
-            src: path + "/images/gamebuilder/sprites/astro.png",
+            src: path + "/images/gamebuilder/sprites/dew.png",
             SCALE_FACTOR: 5,
             STEP_FACTOR: 1000,
-            ANIMATION_RATE: 10,
-            INIT_POSITION: { x: 0, y: 165 },
-            pixels: { height: 770, width: 513 },
+            ANIMATION_RATE: 50,
+            INIT_POSITION: { x: 100, y: 300 },
+            pixels: { height: 256, width: 256 },
             orientation: { rows: 4, columns: 4 },
             down: { row: 0, start: 0, columns: 3 },
-            downRight: { row: 1, start: 0, columns: 3, rotate: Math.PI/16 },
+            downRight: { row: 2, start: 0, columns: 3, rotate: Math.PI/16 },
             downLeft: { row: 0, start: 0, columns: 3, rotate: -Math.PI/16 },
             left: { row: 1, start: 0, columns: 3 },
             right: { row: 2, start: 0, columns: 3 },
             up: { row: 3, start: 0, columns: 3 },
-            upLeft: { row: 2, start: 0, columns: 3, rotate: Math.PI/16 },
+            upLeft: { row: 1, start: 0, columns: 3, rotate: Math.PI/16 },
             upRight: { row: 3, start: 0, columns: 3, rotate: -Math.PI/16 },
             hitbox: { widthPercentage: 0, heightPercentage: 0 },
             keypress: { up: 87, left: 65, down: 83, right: 68 }
             };
 
         const npcData1 = {
-            id: 'ship',
+            id: 'coconut',
             greeting: 'Congratulations, you have found the spaceship!',
             src: path + "/images/gamebuilder/sprites/ufos.png",
             SCALE_FACTOR: 2,
@@ -79,4 +79,4 @@ this.classes = [      { class: GameEnvBackground, data: bgData },
     }
 }
 
-export default GameLevelCoconutl3;
+export default Coconutl3;
